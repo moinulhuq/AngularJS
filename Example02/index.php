4e66457955f2e245c8d89925e13614ee83482942
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2017-11-20 07:45:24 +1300

    Update index.php

--- Example02/index.php
+++ Example02/index.php
@@ -76,3 +76,71 @@ var app = angular.module("myApp", []);
 	});
 
 </script>
+
+
+6. Another example of "ng-controller"
+
+<div ng-app="myApp" ng-controller="personCtrl">
+
+	<input type="text" ng-model="firstName"><br>
+	
+	<input type="text" ng-model="lastName"><br>
+	
+	{{fullName()}}
+
+</div>
+
+<script>
+
+	var app = angular.module('myApp', []);
+
+	app.controller('personCtrl', function($scope) {
+	    $scope.firstName = "John";
+	    $scope.lastName = "Doe";
+	    $scope.fullName = function() {
+	        return $scope.firstName + " " + $scope.lastName;
+	    };
+	});
+
+</script>
+
+
+7. Controllers In External Files
+
+<div ng-app="myApp" ng-controller="personCtrl">
+
+	<input type="text" ng-model="firstName"><br>
+	
+	<input type="text" ng-model="lastName"><br>
+	
+	{{fullName()}}
+
+</div>
+
+<script src="personController.js"></script>
+
+
+8. Another example of Controllers In External Files
+
+<div ng-app="myApp" ng-controller="namesCtrl">
+
+<ul>
+  <li ng-repeat="x in names">
+    {{ x.name + ', ' + x.country }}
+  </li>
+</ul>
+
+</div>
+
+<script src="namesController.js"></script>
+
+namesController.js
+-------------------
+
+angular.module('myApp', []).controller('namesCtrl', function($scope) {
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Hege',country:'Sweden'},
+        {name:'Kai',country:'Denmark'}
+    ];
+});
