891e9cc02dcb61418d405c8dd311d830ab708bd1
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2017-11-20 01:52:12 +1300

    Update index.php

--- Example01/index.php
+++ Example01/index.php
@@ -23,7 +23,27 @@
 </div>
 
 
-4. "ng-model" use for binding. Take input using "ng-model" and to dsiplay that value use either {{ }} or "ng-bind". 
+4. AngularJS "ng-init" for initializing the multiple value.
+
+<div ng-app="" ng-init="quantity=1;cost=5">
+
+	<p>Total in dollar: {{ quantity * cost }}</p>
+
+	<p>Total in dollar: <span ng-bind="quantity * cost"></span></p>
+
+</div>
+
+
+5. To show the variable value inside the <input> tag use "ng-model" in this case "ng-bind" can't display.
+
+<div ng-app="" ng-init="name='Moin'">
+
+	<input ng-model="name">
+
+</div>
+
+
+6. "ng-model" use for binding. Take input using "ng-model" and to dsiplay that value use either {{ }} or "ng-bind". 
 
 <div ng-app="">
 
@@ -36,7 +56,39 @@
 </div>
 
 
-5. AngularJS expression
+7. "ng-model" use for multiple inputs.
+
+<div ng-app="" >
+
+	<input type="number" ng-model="quantity">
+	<input type="number" ng-model="price">
+
+	<p> {{quantity * price}}</p>
+
+</div>
+
+
+8. Validate User Input using "ng-model"
+
+<form ng-app="" name="myForm">
+
+    <input type="email" name="myAddress" ng-model="text">
+
+    <span ng-show="myForm.myAddress.$error.email">Not a valid e-mail address</span>
+
+</form>
+
+
+9. AngularJS best example of binding.
+
+<div ng-app="" ng-init="myColor='green'">
+
+	<input style="background-color:{{myColor}}" ng-model="myColor">
+
+</div>
+
+
+10. AngularJS expression
 
 <div ng-app="">
 
@@ -46,27 +98,7 @@
 </div>
 
 
-6. AngularJS best example of binding.
-
-<div ng-app="" ng-init="myColor='green'">
-
-	<input style="background-color:{{myColor}}" ng-model="myColor">
-
-</div>
-
-
-7. AngularJS "ng-init" for initializing the multiple value.
-
-<div ng-app="" ng-init="quantity=1;cost=5">
-
-	<p>Total in dollar: {{ quantity * cost }}</p>
-
-	<p>Total in dollar: <span ng-bind="quantity * cost"></span></p>
-
-</div>
-
-
-8. AngularJS Objects
+11. AngularJS Objects
 
 <div ng-app="" ng-init="person={firstName:'John',lastName:'Doe'}">
 
@@ -77,7 +109,7 @@
 </div>
 
 
-9. AngularJS Arrays
+12. AngularJS Arrays
 
 <div ng-app="" ng-init="points=[1,15,19,2,40]">
 
